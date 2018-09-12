7e9474f4493b7c67eb03e4c8b8f7a7eedf46334c
Alekay <alekaymusic@gmail.com>
2018-09-12 19:48:27 -0400

    refactor: update functions for ES6

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,17 +1,17 @@
 // all tests placed within $() function so that they don't run until DOM is ready
-$(function() {
+$((() => {
 
     // RSS Feeds test suite
-    describe('RSS Feeds', function() {
+    describe('RSS Feeds', () => {
 
         // test to make sure allFeeds variable has been defined and is not empty
-        it('are defined', function() {
+        it('are defined', () => {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
         // test that loops through each feed in allFeeds object and ensures URL is defined
-        it('urls are defined and not empty', function() {
+        it('urls are defined and not empty', () => {
             for (let i = 0; i < allFeeds.length; i++) {
                 expect(allFeeds[i].url).toBeDefined();
                 expect(allFeeds[i].url.length).not.toBe(0);
@@ -19,7 +19,7 @@ $(function() {
         });
        
         // test that loops through each feed in allFeeds to ensure it has a name defined and name is not empty
-        it('feeds are named and not empty', function() {
+        it('feeds are named and not empty', () => {
             for (let i = 0; i < allFeeds.length; i++) {
                 expect(allFeeds[i].name).toBeDefined();
                 expect(allFeeds[i].name.length).not.toBe(0);
@@ -28,18 +28,18 @@ $(function() {
     });
 
     // the menu test suite
-    describe('The menu', function() {
+    describe('The menu', () => {
         
         const body = document.body;
         const menu = document.querySelector('.menu-icon-link');
 
         // test ensures the menu element is hidden by defualt
-        it('menu is hidden by default', function() {
+        it('menu is hidden by default', () => {
             expect(body.className).toContain('menu-hidden');
         });
 
         //  test ensures the menu changes visibility when the icon is clicked
-        it('menu appears', function() {
+        it('menu appears', () => {
             menu.click();
             expect(body.className).not.toContain('menu-hidden');
 
@@ -49,41 +49,41 @@ $(function() {
     });
 
     // initial entries test suite
-    describe('Initial Entries', function() {
+    describe('Initial Entries', () => {
         
         // handle async
-        beforeEach(function(done) {
-            loadFeed(0, function(){
+        beforeEach(done => {
+            loadFeed(0, () => {
                 done();
             });
         });
         
         // test ensures when loadFeed is called and completes, there is at least one .entry element within .feed container
-        it('has at least one entry in the feed container', function() {
+        it('has at least one entry in the feed container', () => {
             let entries = document.querySelector('.feed').getElementsByClassName('entry').length;
             expect(entries).toBeGreaterThan(0);
         });
     });
 
     // New Feed Selection test suite
-    describe('New Feed Selection', function() {
+    describe('New Feed Selection', () => {
 
         // handle async
         let initialFeed;
-        beforeEach(function(done) {
-            loadFeed(0, function() {
+        beforeEach(done => {
+            loadFeed(0, () => {
                 initialFeed = document.querySelector('.feed').innerHTML;
 
-                loadFeed(1, function() {
+                loadFeed(1, () => {
                     done();
                 });
             });
         });
 
         // test ensures when a new feed is loaded by loadFeed that the content changes.
-        it('changes content on new feed load', function() {
+        it('changes content on new feed load', () => {
             let newFeed = document.querySelector('.feed').innerHTML;
             expect(initialFeed).not.toBe(newFeed);
         });
     });    
-}());
+})());
